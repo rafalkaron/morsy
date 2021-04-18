22e38d2fba85624d85036c58b79a4cc7414f5f04
rafalkaron <rafalkaron@gmail.com>
2021-04-18 19:21:15 +0200

    update

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const plaintextInput = document.getElementById("plaintextInput");
 const btnGenerate = document.querySelector(".btn-generate");
 const morseOutput = document.getElementById("morseOutput");
 
+const generateMorse = (inputText) => {
+  const lowerCased = text.toLowerCase(text);
+  console.log(lowerCased);
+};
+
 btnGenerate.addEventListener("click", function () {
-  console.log(plaintextInput.value);
+  console.log("[i] Generate Morse");
   morseOutput.innerHTML = plaintextInput.value;
+  //morseOutput.innerHTML = generateMorse(plaintextInput.value);
 });
