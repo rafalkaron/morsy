3c3a4a9a56703c15e0e246756bd736ff2dd20a44
rafalkaron <rafalkaron@gmail.com>
2021-04-18 19:38:08 +0200

    A encoded

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const plaintextInput = document.getElementById("plaintextInput");
 const btnGenerate = document.querySelector(".btn-generate");
 const morseOutput = document.getElementById("morseOutput");
 
-const generateMorse = (inputText) => {
-  const lowerCased = text.toLowerCase(text);
-  console.log(lowerCased);
+/* Uppercase all characters */
+const upperCase = (inputText) => {
+  const upperCased = inputText.toUpperCase(inputText);
+  console.log(`[Uppercase] ${upperCased}`);
+  return upperCased;
 };
 
+const encodeCharacters = (inputText) => {
+  const encoded = inputText.replace("A", ".-");
+  console.log(`[Encode] ${encoded}`);
+  return encoded;
+};
+
+/* Generate Morse code */
+const generateMorse = (inputText) => {
+  var morse = upperCase(inputText);
+  var morse = encodeCharacters(morse);
+  return morse;
+};
+
+/* Populate the output field on click */
 btnGenerate.addEventListener("click", function () {
   console.log("[i] Generate Morse");
-  morseOutput.innerHTML = plaintextInput.value;
-  //morseOutput.innerHTML = generateMorse(plaintextInput.value);
+  //morseOutput.innerHTML = plaintextInput.value;
+  morseOutput.innerHTML = generateMorse(plaintextInput.value);
 });
