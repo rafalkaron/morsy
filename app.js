f531eefe84e30873d44df463bda903df378de57b
Rafał Karoń <rafalkaron@gmail.com>
2021-04-18 21:29:17 +0200

    Js backend (#2)
    * Value saved to memory
    
    * input passed to output
    
    * update
    
    * A encoded
    
    * added dictionary
    
    * Juz mi sie robi mors

--- app.js
+++ app.js
@@ -1,3 +1,87 @@
-var plaintextInput = document.getElementById("plaintextInput").value;
+const plaintextInput = document.getElementById("plaintextInput");
+const btnGenerate = document.querySelector(".btn-generate");
+const morseOutput = document.getElementById("morseOutput");
 
-console.log(plaintextInput);
+/* Character:Morse Dictionary */
+const morseDictionary = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+
+  ".": ".-.-.-",
+  "?": "..--..",
+  "!": "-.-.--",
+  "(": "-.--.",
+  ")": "-.--.-",
+  ":": "---...",
+  ";": "-.-.-.",
+  ",": "--..--",
+  "'": ".----.",
+  "/": "-..-.",
+  "&": ".-...",
+  "-": "-....-",
+  " ": "/", //issue with whitespace
+};
+
+/* Uppercase all characters */
+const upperCase = (inputText) => {
+  const upperCased = inputText.toUpperCase(inputText);
+  console.log(`[Uppercase] ${upperCased}`);
+  return upperCased;
+};
+
+/* Encode uppercased characters */
+const encodeCharacters = (inputText) => {
+  // let encoded = inputText.replace("A", ".-");
+  let encoded = inputText.replace(/./gi, (m) => morseDictionary[m]);
+  console.log(`[Encode] ${encoded}`);
+  return encoded;
+};
+
+/* Generate Morse code */
+const generateMorse = (inputText) => {
+  let morse = upperCase(inputText);
+  morse = encodeCharacters(morse);
+  return morse;
+};
+
+/* Populate the output field on click */
+btnGenerate.addEventListener("click", function () {
+  console.log("[i] Generate Morse");
+  //morseOutput.innerHTML = plaintextInput.value;
+  morseOutput.innerHTML = generateMorse(plaintextInput.value);
+});
