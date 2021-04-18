6ead91b3731f76bf6a36a66657920865f76a7990
rafalkaron <rafalkaron@gmail.com>
2021-04-18 19:44:07 +0200

    added dictionary

--- app.js
+++ app.js
@@ -2,6 +2,61 @@ const plaintextInput = document.getElementById("plaintextInput");
 const btnGenerate = document.querySelector(".btn-generate");
 const morseOutput = document.getElementById("morseOutput");
 
+/* Character:Morse Dictionary */
+const morseDictionary = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+
+  ".": ".-.-.-",
+  "?": "..--..",
+  "!": "-.-.--",
+  "(": "-.--.",
+  ")": "-.--.-",
+  ":": "---...",
+  ";": "-.-.-.",
+  ",": "--..--",
+  "'": ".----.",
+  "/": "-..-.",
+  "&": ".-...",
+  "-": "-....-",
+  " ": "/",
+};
+
 /* Uppercase all characters */
 const upperCase = (inputText) => {
   const upperCased = inputText.toUpperCase(inputText);
@@ -9,6 +64,7 @@ const upperCase = (inputText) => {
   return upperCased;
 };
 
+/* Encode uppercased characters */
 const encodeCharacters = (inputText) => {
   const encoded = inputText.replace("A", ".-");
   console.log(`[Encode] ${encoded}`);
